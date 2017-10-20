54e7975da6315a6965e1db931efbf448f0ae8eca
hitekvn <31841044+hitekvn@users.noreply.github.com>
2017-10-20 21:00:28 +0700

    #2

---
+++ QuanLyThuVien/Functions/LoginF.cs
@@ -0,0 +1,23 @@
+﻿using QuanLyThuVien.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyThuVien.Functions
+{
+    class LoginF
+    {
+        private LibraryManagementEntities db = new LibraryManagementEntities();
+        public IQueryable<Login> Logins
+        {
+            get { return db.Logins; }
+        }
+        public bool Check(string username, string password)
+        {
+            Login dbEntry = db.Logins.Where(x => x.UserName == username && x.Password == password).FirstOrDefault();
+            return dbEntry != null;
+        }
+    }
+}
