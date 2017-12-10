0df1edd50a14f031008c55c0706d7df03af28018
trungkien9996 <31985809+trungkien9996@users.noreply.github.com>
2017-12-10 07:56:38 +0700

    ads

--- QuanLyThuVien/Functions/VoucherF.cs
+++ QuanLyThuVien/Functions/VoucherF.cs
@@ -19,5 +19,22 @@ namespace QuanLyThuVien.Functions
             Voucher dbEntry = db.Vouchers.Where(x => x.ID == id).FirstOrDefault();
             return dbEntry != null;
         }
+        public bool Insert(Voucher model)
+        {
+            Book bookEntry = db.Books.Where(x => x.ID == model.BookID).FirstOrDefault();
+            bookEntry.Quantity = (int)bookEntry.Quantity - (int)model.Quantity;
+            db.Vouchers.Add(model);
+            db.SaveChanges();
+            return true;
+        }
+        public bool Delete(int id)
+        {
+            Voucher dbEntry = db.Vouchers.Where(x => x.ID == id).FirstOrDefault();
+            Book bookEntry = db.Books.Where(x => x.ID == dbEntry.BookID).FirstOrDefault();
+            bookEntry.Quantity = bookEntry.Quantity + (int)dbEntry.Quantity;
+            db.Vouchers.Remove(dbEntry);
+            db.SaveChanges();
+            return true;
+        }
     }
 }
